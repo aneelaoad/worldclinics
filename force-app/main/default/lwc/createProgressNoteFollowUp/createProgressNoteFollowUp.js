3c90a4ed7c65e0884212234cf817091151334637
aoad1 <aoad@jobsitecare.com>
2024-07-25 21:43:42 +0500

    added project

---
+++ force-app/main/default/lwc/createProgressNoteFollowUp/createProgressNoteFollowUp.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+export default class CreateProgressNoteFollowUp extends LightningElement {
+
+    //----FLAGS-------
+    showLoader = false;
+    showPNForm = false;
+    showFUForm = false;
+
+
+    selectedRecordTypeId = '';
+    selectedRecordTypeName = '';
+
+}
